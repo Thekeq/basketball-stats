278390ff707aca3140508662b3cb15552c331d2c
TheKeq <sepelamisa@gmail.com>
2025-07-01 23:37:31 +0300

    Update text

--- text.py
+++ text.py
@@ -5,7 +5,8 @@ msg = {"start_en": ("Hi! I'm a bot that helps you track your basketball performa
                     "/threepoint_stats — view your three-point stats\n"
                     "/delete_shot — delete any unwanted data\n"
                     "/avg_stats — check your average made shots\n"
-                    "/language — change the language"),
+                    "/language — change the language\n\n"
+                    "Taken from https://github.com/Thekeq/basketball-stats"),
 
        "start_ru": ("Привет! Я бот для отслеживания твоих баскетбольных результатов.\n"
                     "Вот список доступных команд:\n"
@@ -14,7 +15,8 @@ msg = {"start_en": ("Hi! I'm a bot that helps you track your basketball performa
                     "/threepoint_stats — статистика по трёшкам\n"
                     "/delete_shot — удалить ненужные данные\n"
                     "/avg_stats — узнать среднее количество забитых\n"
-                    "/language — поменять язык"),
+                    "/language — поменять язык\n\n"
+                    "Взято с https://github.com/Thekeq/basketball-stats"),
 
        "language_en": ("Language changed! 🇺🇸"),
 
